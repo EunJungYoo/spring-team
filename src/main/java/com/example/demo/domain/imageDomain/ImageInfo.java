1efa1af3f6b39109fbf0150023d9090510bbdf65
Heylim Kim <hlim9022@gmail.com>
2022-08-06 10:13:16 +0900

    aws S3 연결시작

---
+++ src/main/java/com/example/demo/domain/imageDomain/ImageInfo.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain.imageDomain;
+
+public class ImageInfo {
+    private String id; // UUID 값으로 설정하기 위함
+    private String imageName; // 업로드시점 파일명
+    private String imageFormat; // 업로드시점 파일명
+    private String path; // 파일의 실제 물리적 경로값
+    private long bytes;
+
+
+
+
+
+}
