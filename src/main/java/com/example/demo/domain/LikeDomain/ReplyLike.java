a60640a1a4baf523289dac03905c53d1f2f89b26
cheoljun <ljc3484@naver.com>
2022-08-09 21:42:27 +0900

    ReplyLike,java 수정

--- src/main/java/com/example/demo/domain/LikeDomain/ReplyLike.java
+++ src/main/java/com/example/demo/domain/LikeDomain/ReplyLike.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.LikeDomain;
 
 import com.example.demo.domain.Reply;
 import com.example.demo.domain.User;
+import com.example.demo.domain.dto.likeDto.ReplyLikeDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,9 +25,13 @@ public class ReplyLike {
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-    @JoinColumn(name = "post_id", nullable = false)
+    @JoinColumn(name = "reply_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Reply reply;
 
 
+    public ReplyLike(ReplyLikeDto replyLikeDto) {
+        this.user = replyLikeDto.getUser();
+        this.reply = replyLikeDto.getReply();
+    }
 }
